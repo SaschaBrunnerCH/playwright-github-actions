cf98bdf42b8da389a004f24f8201db9db4007ff5
Sascha Brunner <sbrunner@esri.com>
2020-10-23 23:41:23 +0200

    aquarium

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {chromium, firefox} = require('playwright');
         viewport: { width: 730, height: 550 },
       });
     const page = await context.newPage();
-    await page.goto('https://webglreport.com/');
+    await page.goto('https://webglsamples.org/aquarium/aquarium.html');
     await page.waitForTimeout(10000);
     await page.screenshot({
       path: `screenshot-${browserType.name()}.png`,
